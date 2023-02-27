b063eba8c368e81e84a1928cf4b7ce470254becf
sinestergates <104525142+sinestergates@users.noreply.github.com>
2023-02-27 13:11:51 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TypesExample from './but'
+
+ReactDOM.render(
+  
+  <div>
+    <TypesExample></TypesExample>
+  </div>,
+  
+document.getElementById('root')
+);
+
